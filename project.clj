e6b5977433ae726ca339e66a8e9b93fea62d0419
Mayank Jain <mayank@helpshift.com>
2013-06-14 18:33:56 +0530

    Updated Clojure version to 1.5.1. Replaced all :use only -> :require refer. Add split to refer and import java.io.
    Change-Id: Ia94e3626a8ad22f46e77819b96ef3b801e87d333

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.2"]
                  [org.clojure/data.json "0.2.2"]])
--- src/sauce_api/utils.clj
+++ src/sauce_api/utils.clj
@@ -2,9 +2,10 @@
       :author "Mayank Jain <mayank@helpshift.com>"}
   sauce-api.utils
   (:require [sauce-api.clj-http-extended :as clje]
-            [clojure.data.json :as json])
-  (:use [clojure.java.io :only (make-output-stream)]
-        [clojure.string :only (join)]))
+            [clojure.data.json :as json]
+            [clojure.java.io :refer [make-output-stream file]]
+            [clojure.string :refer [join split]])
+  (:import [java.io File]))
 
 (def base-url "https://saucelabs.com/rest/v1/")
 
@@ -24,7 +25,7 @@
   [usr access-key job-id path f]
   (let [dir-path (str path "/" job-id)]
     (println (format "Thread %s: Creating directory: %s" (get-thread-id) dir-path))
-    (.mkdirs (java.io.File. dir-path))
+    (.mkdirs (File. dir-path))
     (println (format "Thread %s: Downloading %s ..." (get-thread-id) f))
     (let [res (clje/clj-get (str base-url usr "/jobs/" job-id "/assets/" f)
                             {:basic-auth [usr access-key]
@@ -43,10 +44,10 @@
 (defn get-filename
   "Returns the filename from the given file-path."
   [file-path]
-  (last (clojure.string/split file-path #"/")))
+  (last (split file-path #"/")))
 
 (defn get-all-files-path
   "Returns a seq of paths for all the files in a given directory."
   [dir-path]
-  (for [file (rest (file-seq (clojure.java.io/file dir-path)))]
+  (for [file (rest (file-seq (file dir-path)))]
     (.getPath file)))
